57af54a38ab03900a9f9c6f3da08084ebd2467a2
heweifeng <heweifeng@autohome.com.cn>
2017-07-25 18:59:59 +0800

    连接测试失败,TODO: domain实现序列化

--- src/main/java/vip/hewe/service/client/data/AccountClient.java
+++ src/main/java/vip/hewe/service/client/data/AccountClient.java
@@ -1,15 +1,9 @@
 package vip.hewe.service.client.data;
 
-import grpc.service.MemberDataServiceGrpc;
-import grpc.service.MemberIdMsg;
-import grpc.service.MemberMsg;
-import grpc.service.RecordMsg;
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
-import io.grpc.Status;
-import io.grpc.StatusRuntimeException;
+import jdk.nashorn.internal.ir.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import vip.hewe.api.service.SignService;
 
 /**
  *
@@ -17,39 +11,10 @@ import org.slf4j.LoggerFactory;
 public class AccountClient {
     private static final Logger logger = LoggerFactory.getLogger(AccountClient.class);
 
-    private final ManagedChannel channel;
-    private final MemberDataServiceGrpc.MemberDataServiceBlockingStub blockingStub;
+    @Reference
+    private SignService signService;
 
-    public AccountClient(String host, int port) {
 
-        this.channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext(true).build();
-        this.blockingStub = MemberDataServiceGrpc.newBlockingStub(channel);
-    }
 
-    public int createMember(MemberMsg msg) {
-        RecordMsg recordMsg = blockingStub.insert(msg);
-        int count = recordMsg.getCount();
-        return count;
-    }
 
-    public MemberMsg select(String id) {
-        MemberMsg msg = null;
-        try {
-            msg = blockingStub.selectById(MemberIdMsg.newBuilder().setId(id).build());
-        } catch (StatusRuntimeException sexc) {
-            msg = null;
-            Status status = sexc.getStatus();
-            switch (status.getCode()) {
-                case NOT_FOUND:
-                    msg = null;
-                    break;
-                default:
-                    sexc.printStackTrace();
-                    break;
-            }
-        } catch (Exception ee) {
-            ee.printStackTrace();
-        }
-        return msg;
-    }
 }
